e1f4d4574d71fe5baa08dccdd0f2d73331c9b2c1
priya2771 <157040256+priya2771@users.noreply.github.com>
2025-01-04 22:31:15 +0530

    cursor

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+  const postX = e.clientX;
+  const postY = e.clientY;
+
+  cursorDot.style.left = `${postX}px`;
+  cursorDot.style.top = `${postY}px`;
+
+  // cursorOutline.style.left = `${postX}px`;
+  // cursorOutline.style.top = `${postY}px`;
+  cursorOutline.animate(
+    {
+      left: `${postX}px`,
+      top: `${postY}px`,
+    },
+    { duration: 500, fill: "forwards" }
+  );
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   const inputBox = document.getElementById("inputbox");
   const buttons = document.querySelectorAll("button");
